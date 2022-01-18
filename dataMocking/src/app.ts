7a6bb37dbb56f6ae9f786b89f439beee37d0d5f9
sj.park <sj.park1@lotte.net>
2022-01-18 16:38:13 +0900

    feat: read, create API 개발

--- dataMocking/src/app.ts
+++ dataMocking/src/app.ts
@@ -1,30 +1,80 @@
 import * as express from 'express';
-import { Animal, AnimalType } from './app.model';
+import { Animal, AnimalType } from './animal/animals.model';
 
 const app: express.Express = express();
 
 const data = [1, 2, 3, 4];
 
+// logging middleware
 app.use((req, res, next) => {
   console.log('logging middleware');
   next();
 });
 
-app.get('/', (req: express.Request, res: express.Response) => {
-  console.log(req.rawHeaders[1]);
-  res.send({ animals: Animal });
+// json middleware: post로 입력받는 데이터(json)를 식별하기 위한 미들웨어
+app.use(express.json());
+
+// Read all data
+app.get('/animals', (req, res) => {
+  try {
+    const animals = Animal;
+    res.status(200).send({
+      success: true,
+      data: {
+        animals,
+      },
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
 });
 
-app.get('/cat', (req, res) => {
-  console.log('cat data');
-  res.send({ cat: Animal[2] });
+// Read one data
+app.get('/animal/:id', (req, res) => {
+  try {
+    const id = req.params.id;
+    const cat = Animal.find((cat) => {
+      return cat.id === id;
+    });
+    res.status(200).send({
+      success: true,
+      data: {
+        cat,
+      },
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
 });
 
-app.get('/dog', (req, res) => {
-  console.log('dog data');
-  res.send({ dog: Animal[0] });
+//Create animal
+app.post('/animal', (req, res) => {
+  try {
+    const data = req.body;
+    console.log(data);
+    Animal.push(data);
+
+    res.status(200).send({
+      success: true,
+      data: {
+        Animal,
+      },
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
 });
 
+// 404 middleware
 app.use((req, res, next) => {
   console.log('error middleware');
   res.send({ error: '404 not found error' });
