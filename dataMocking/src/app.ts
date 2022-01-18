05097e6f2e929060d128816950544aa42e6e4ed7
sj.park <sj.park1@lotte.net>
2022-01-18 13:32:53 +0900

    feat: express middleware 적용

--- dataMocking/src/app.ts
+++ dataMocking/src/app.ts
@@ -5,9 +5,29 @@ const app: express.Express = express();
 
 const data = [1, 2, 3, 4];
 
+app.use((req, res, next) => {
+  console.log('logging middleware');
+  next();
+});
+
 app.get('/', (req: express.Request, res: express.Response) => {
   console.log(req.rawHeaders[1]);
   res.send({ animals: Animal });
+});
+
+app.get('/cat', (req, res) => {
+  console.log('cat data');
+  res.send({ cat: Animal[2] });
+});
+
+app.get('/dog', (req, res) => {
+  console.log('dog data');
+  res.send({ dog: Animal[0] });
+});
+
+app.use((req, res, next) => {
+  console.log('error middleware');
+  res.send({ error: '404 not found error' });
 });
 
 app.listen(8000, () => {
