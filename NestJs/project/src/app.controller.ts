029217e61959e462f5485c4f77798302e8b24113
park so jeong <psj6414@naver.com>
2022-01-26 16:35:56 +0900

    feat: controller 작성
    - post, get 기능 구현

--- NestJs/project/src/app.controller.ts
+++ NestJs/project/src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Req } from '@nestjs/common';
+import { Request } from 'express';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Post('user')
+  insertUser(@Body() body): string {
+    console.log(body);
+    return 'insert user test';
+  }
+
+  @Get('user/:id')
+  getHello(@Req() req: Request, @Body() body, @Param() param): string {
+    console.log(param);
     return this.appService.getHello();
   }
 }
